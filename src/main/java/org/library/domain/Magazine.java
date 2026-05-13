f28302246965741b52fea4e482c93f6781e5923d
SavulescuD <savulescud13@gmail.com>
2026-05-13 11:28:20 -0400

    Started the Magazine class

---
+++ src/main/java/org/library/domain/Magazine.java
@@ -0,0 +1,21 @@
+package org.library.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Magazine extends Item {
+    private int issueNumber;
+    private String publisher;
+
+    public Magazine(String title, Status status, int issueNumber, String publisher) {
+        super(title, status);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
+}
