d322d95c0743322fc863dea95b19adb5f37c722d
SavulescuD <savulescud13@gmail.com>
2026-05-13 10:54:23 -0400

    Started the User class

---
+++ src/main/java/org/library/domain/User.java
@@ -0,0 +1,28 @@
+package org.library.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public abstract class User {
+    protected String id;
+    @Setter
+    protected String name;
+    @Setter
+    protected List<Item> items;
+
+    private static int nextId = 1;
+
+    public User(String name) {
+        this.id = String.format("%04d", nextId++);
+        this.name = name;
+        this.items = new ArrayList<>();
+    }
+}
