79be173cf6eabfbe9b2eb67760ceffa665419eed
SavulescuD <savulescud13@gmail.com>
2026-05-14 18:04:56 -0400

    Updated the Library file

--- src/main/java/org/library/domain/Library.java
+++ src/main/java/org/library/domain/Library.java
@@ -4,9 +4,13 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.library.util.Constants;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 @Getter
 @Setter
@@ -14,6 +18,7 @@ import java.util.List;
 @EqualsAndHashCode
 public class Library {
     public static List<Item> items;
+    public static List<User> users;
 
     /**
      * permits the user to borrow items
@@ -46,7 +51,6 @@ public class Library {
 
     /**
      * permits the user to return an item
-     *
      * @param item the item to be returned
      * @param user the user returning the item
      */
@@ -56,10 +60,16 @@ public class Library {
         item.setStatus(Item.Status.AVAILABLE);
     }
 
+    /**
+     * enables the user to search for an item using a key word using a stream
+     * @param keyWord the key word used for the search
+     * @return a list of the items containing the key word
+     */
     public List<Item> searchStream(String keyWord) {
         String lowerCaseKey = keyWord.toLowerCase();
 
         List<Item> searchedItems = new ArrayList<>(items);
+        Set<String> seenTitles = new TreeSet<>();
 
         return searchedItems.stream()
                 .filter(item -> {
@@ -67,8 +77,176 @@ public class Library {
                     if (item instanceof Book) {
                         authorContainsKeyWord = ((Book) item).getAuthor().toLowerCase().contains(lowerCaseKey);
                     }
+                    seenTitles.add(item.getTitle().toLowerCase());
                     return item.getTitle().toLowerCase().contains(lowerCaseKey) || authorContainsKeyWord;
                 })
+                .filter(item -> {
+                    return (!seenTitles.contains(item.getTitle()));
+                })
                 .toList();
+    }
+
+    /**
+     * enables the user to search for an item using a key word using recursion
+     * @param keyword the keyword to search for the item
+     * @param index the index to check
+     * @param results the results of the search so far
+     * @param seenTitles the titles that have already been checked
+     * @return the list of all the items containing that key word
+     */
+    public List<Item> searchRecursive(String keyword, int index, List<Item> results, Set<String> seenTitles) {
+        if (index == items.size()) {
+            return results;
+        }
+
+        Item currentItem = items.get(index);
+        String lowerKeyword = keyword.toLowerCase();
+        String title = currentItem.getTitle().toLowerCase();
+
+        boolean matches = title.contains(lowerKeyword);
+        if (!matches && currentItem instanceof Book) {
+            matches = ((Book) currentItem).getAuthor().toLowerCase().contains(lowerKeyword);
+        }
+
+        if (matches && !seenTitles.contains(title)) {
+            results.add(currentItem);
+            seenTitles.add(title);
+        }
+
+        return searchRecursive(keyword, index + 1, results, seenTitles);
+    }
+
+    /**
+     * initializes the users from a user file
+     * @param userPath the path for the user file
+     */
+    public void initializeUsers(String userPath) {
+        File file = new File(userPath);
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] elements = line.split(",");
+
+                /*
+                structure for a line should be like so: Occupation,Name
+                example -> Student, Name
+                 */
+                for (String str : elements) {
+                    String name = elements[1];
+                    if (elements[0].equalsIgnoreCase("student")) {
+                        Student student = new Student(name);
+                        users.add(student);
+                    } else if (elements[0].equalsIgnoreCase("teacher")) {
+                        Teacher teacher = new Teacher(name);
+                        users.add(teacher);
+                    } else if (elements[0].equalsIgnoreCase("admin")) {
+                        Admin admin = new Admin(name);
+                    } else {
+                        throw new RuntimeException("User indeterminable");
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * initializes the items from a user file
+     * @param itemsPath the path for the item file
+     */
+    public void initializeItems(String itemsPath) {
+        File file = new File(itemsPath);
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] elements = line.split(",");
+
+                /*
+                structure for a line should be like so:
+                Book,title,status,isbn,author,genre
+                Dvd,title,status,director,duration
+                Magazine,title,status,issueNumber,publisher
+                example -> Student, Name
+                 */
+                for (String str : elements) {
+                    String title = elements[1].toLowerCase();
+                    Item.Status status;
+
+                    if (elements[2].equalsIgnoreCase("borrowed")) {
+                        status = Item.Status.BORROWED;
+                    } else if (elements[2].equalsIgnoreCase("available")) {
+                        status = Item.Status.AVAILABLE;
+                    } else {
+                        status = Item.Status.LOST;
+                    }
+
+                    switch(elements[0]) {
+                        case "book" -> {
+                            Book book = new Book(title, status, elements[3], elements[4], elements[5]);
+                            items.add(book);
+                        }
+
+                        case "dvd" -> {
+                            Dvd dvd = new Dvd(title, status, elements[3], Integer.parseInt(elements[4]));
+                            items.add(dvd);
+                        }
+
+                        case "magazine" -> {
+                            Magazine magazine = new Magazine(title, status, Integer.parseInt(elements[3]), elements[4]);
+                            items.add(magazine);
+                        }
+
+                        default -> throw new RuntimeException("Item indeterminable");
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadUsers() {
+        File file = new File(Constants.USERS_CSV_PATH);
+
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            for (User user : users) {
+                if (user instanceof Student) {
+                    fileWriter.write("Student,");
+                } else if (user instanceof Teacher) {
+                    fileWriter.write("Teacher,");
+                } else {
+                    fileWriter.write("Admin,");
+                }
+                fileWriter.write(user.getId() + ",");
+                fileWriter.write(user.getName() + ",");
+                fileWriter.write("Borrowed Items:");
+                for (Item item : user.getUserBorrowedItems()) {
+                    fileWriter.write(item.getId() + "-" + item.getTitle());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadBooks() {
+        File file = new File(Constants.BOOKS_CSV_PATH);
+
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            for (Item item : items) {
+                if (item instanceof Book) {
+                    fileWriter.write(item.getId() + ",");
+                    fileWriter.write(item.getTitle() + ",");
+                    fileWriter.write(((Book) item).getIsbn() + ",");
+                    fileWriter.write(((Book) item).getAuthor() + ",");
+                    fileWriter.write(((Book) item).getGenre() + ",");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
