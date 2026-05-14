b6d813990bdde392fc16ca2e1e572abbd1bdf6b5
SavulescuD <savulescud13@gmail.com>
2026-05-14 18:04:41 -0400

    Updated the Constants file

--- src/main/java/org/library/util/Constants.java
+++ src/main/java/org/library/util/Constants.java
@@ -3,4 +3,6 @@ package org.library.util;
 public class Constants {
     public static final int MAX_BOOKS_PER_STUDENT = 5;
     public static final int MAX_ITEMS_PER_TEACHER = 10;
+    public static final String BOOKS_CSV_PATH = "src/main/resources/books.csv";
+    public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
 }
