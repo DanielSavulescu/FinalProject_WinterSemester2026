ca82a1119666eec682c3d2ac4b4061b09c019d91
SavulescuD <savulescud13@gmail.com>
2026-05-14 09:42:36 -0400

    Implemented the Reportable Interface

--- src/main/java/org/library/domain/Admin.java
+++ src/main/java/org/library/domain/Admin.java
@@ -1,13 +1,58 @@
 package org.library.domain;
 
 import lombok.*;
+import org.library.interfaces.Reportable;
 
 @ToString(callSuper = true)
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = true)
-public class Admin extends User {
+public class Admin extends User implements Reportable {
     public Admin(String name) {
         super(name);
+    }
+
+    @Override
+    public void reportInStore() {
+        for (Item item : Library.items) {
+            if (item.getStatus().equals(Item.Status.AVAILABLE)) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    @Override
+    public void reportBorrowed() {
+        for (Item item : Library.items) {
+            if (item.getStatus().equals(Item.Status.BORROWED)) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    @Override
+    public void reportLost() {
+        for (Item item : Library.items) {
+            if (item.getStatus().equals(Item.Status.LOST)) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    @Override
+    public void reportALl() {
+        for (Item item : Library.items) {
+            if (item.getStatus().equals(Item.Status.AVAILABLE)) {
+                System.out.println(item.getId() + "-" + item.getTitle() + ": Available");
+            }
+
+            if (item.getStatus().equals(Item.Status.BORROWED)) {
+                System.out.println(item.getId() + "-" + item.getTitle() + ": Borrowed");
+            }
+
+            if (item.getStatus().equals(Item.Status.LOST)) {
+                System.out.println(item.getId() + "-" + item.getTitle() + ": Lost");
+            }
+        }
     }
 }
