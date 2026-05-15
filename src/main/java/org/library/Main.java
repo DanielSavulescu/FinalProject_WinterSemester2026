40fd1b3ffeb0bd9955a1a0a0c177dc4b1d4f839c
SavulescuD <savulescud13@gmail.com>
2026-05-14 21:36:56 -0400

    Finished the Main file

--- src/main/java/org/library/Main.java
+++ src/main/java/org/library/Main.java
@@ -1,17 +1,32 @@
 package org.library;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.library.domain.*;
+
+import java.util.ArrayList;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        Library.items = new ArrayList<>();
+        Library.users = new ArrayList<>();
+        Book book1 = new Book("Java101", Item.Status.BORROWED, "1234567891023", "Steve", "Coding");
+        Book book2 = new Book("Python101", Item.Status.AVAILABLE, "1234567893023", "Steve", "Coding");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        Library.items.add(book1);
+        Library.items.add(book2);
+
+        Student student1 = new Student("Bobby");
+        Student student2 = new Student("Jon");
+        Teacher teacher1 = new Teacher("Johnny");
+        Teacher teacher2 = new Teacher("Emma");
+        Admin admin = new Admin("Daniel");
+
+        Library.users.add(student1);
+        Library.users.add(student2);
+        Library.users.add(teacher1);
+        Library.users.add(teacher2);
+        Library.users.add(admin);
+        Library.borrowItems(book2, student1);
+        Library.loadUsers();
+        Library.loadBooks();
     }
 }
